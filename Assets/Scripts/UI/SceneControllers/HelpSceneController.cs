cef90355b086f2958e2b96cf7c7f12d62a55f665
Seroster <ccamfield@gmail.com>
2014-08-28 18:38:34 -0400

    Updated help with images and multiple pages

--- Assets/Scripts/UI/SceneControllers/HelpSceneController.cs
+++ Assets/Scripts/UI/SceneControllers/HelpSceneController.cs
@@ -3,9 +3,23 @@ using System.Collections;
 
 public class HelpSceneController : RSSceneController 
 {
+	[SerializeField] private GameObject[] helpContents;
+	
+	private int currentHelp = 0;
 
-	private void OnButtonPressed()
+	public void OnHelpButtonPressed()
 	{
+		// turn off the current help, turn on the next help... or if there is no help left, go to the main menu
+		
+		this.helpContents[this.currentHelp].SetActive(false);
+		
+		this.currentHelp++;
+		if (this.currentHelp < this.helpContents.Length)
+		{
+			this.helpContents[this.currentHelp].SetActive(true);
+			return;
+		}
+		
 		Managers.SceneManager.LoadScene(SerpentConsts.SceneNames.Main);
 	}
 
